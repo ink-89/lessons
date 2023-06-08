a9eca2dc91f48ee9fd39d751322b9f282f43f494
ink-89 <123498541+ink-89@users.noreply.github.com>
2023-06-08 17:32:02 +0300

    Add files via upload

---
+++ Lesson_2JS/js/script.js
@@ -0,0 +1,21 @@
+"use strict"
+																				// УРОК № 2
+// let a=3
+// let b=90
+// let s = a+b
+// alert('sum =' + s)
+
+// alert(`Sum=${s}`)
+// document.write(`Sum=${s}`)
+
+// let userName = prompt('Як Вас звати?', 'Гість!')
+// document.write(`Привіт ${userName} на сайті!`)
+
+// let a = parseInt(prompt('First number', '0'))
+// let boxCount = parseInt(prompt('Кіл-ть ящиків'))
+// let boxWeight = parseFloat(prompt('Вага одного ящика'))
+// let sum = boxCount * boxWeight
+// document.write(`Total weight = ${sum.toFixed(2)}`)
+
+
+
